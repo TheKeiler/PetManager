1a7f7b70aacde47b841567929e3317aaff61b9b0
Marco <marco.wetli@gmail.com>
2018-09-02 14:43:05 +0200

    testMarco

--- PetManager/PetManager/Pet.cs
+++ PetManager/PetManager/Pet.cs
@@ -9,6 +9,13 @@ namespace PetManager {
     public class Pet {
         public string Name { get; set; }
         public string Breed { get; set; }
+        public string Birthday { get; set; }
+        public int Age { get; set; }
+
+        public Pet(String Birthday)
+        {
+            this.Birthday = Birthday;
+        }
 
         public int GetHashCode()
         {
