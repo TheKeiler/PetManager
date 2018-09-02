79838e3ee5b7671b9c7a7841fc9a80e79f82a23a
Patrick Keist <p.keist@hotmail.com>
2018-09-02 13:25:47 +0200

    implemented GetHash in Class Pet

--- PetManager/PetManager/Pet.cs
+++ PetManager/PetManager/Pet.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
 
 namespace PetManager {
     public class Pet {
         public string Name { get; set; }
         public string Breed { get; set; }
+
+        public int GetHashCode()
+        {
+            int hash = 33;
+            hash = hash * 12 + (this.Name != null ? this.Name.GetHashCode() : 0);
+            hash = hash * 12 + (this.Breed != null ? this.Breed.GetHashCode() : 0);
+            return hash;
+        }
     }
 }
